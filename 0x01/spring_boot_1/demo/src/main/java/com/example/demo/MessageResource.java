4bbdbb1117957d59c4338ee0b1af73f334e6d86f
Cloik <claudineialoik@gmail.com>
2025-11-11 23:22:19 -0300

    Task 0 - java microsservico - v01

---
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class MessageResource {
+
+    // Endpoint simples de boas-vindas
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    // Endpoint de login
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String user,
+                        @RequestParam(required = false) String password) {
+
+        // Regra de negócio
+        if (user == null || user.isBlank() || password == null || password.isBlank()) {
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        }
+
+        if (user.length() > 15 || password.length() > 15) {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+
+        return "LOGIN EFETUADO COM SUCESSO !!!";
+    }
+}
